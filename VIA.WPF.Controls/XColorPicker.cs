326dea1b57c36d0a23e04890c3f8ffd2976df9ad
Claus <FalkensteinVIA@gmx.de>
2026-07-28 14:31:35 +0200

    Refine XColorPicker editing controls

--- VIA.WPF.Controls/XColorPicker.cs
+++ VIA.WPF.Controls/XColorPicker.cs
@@ -10,24 +10,74 @@ public class XColorPicker : Control
     private bool synchronizing;
 
     public static readonly DependencyProperty SelectedColorProperty = DependencyProperty.Register(
-        nameof(SelectedColor), typeof(Color), typeof(XColorPicker), new FrameworkPropertyMetadata(Colors.Transparent, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, OnColorChanged));
-    public static readonly DependencyProperty RedProperty = RegisterChannel(nameof(Red));
-    public static readonly DependencyProperty GreenProperty = RegisterChannel(nameof(Green));
-    public static readonly DependencyProperty BlueProperty = RegisterChannel(nameof(Blue));
-    public static readonly DependencyProperty AlphaProperty = RegisterChannel(nameof(Alpha));
+        nameof(SelectedColor),
+        typeof(Color),
+        typeof(XColorPicker),
+        new FrameworkPropertyMetadata(
+            Colors.Black,
+            FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+            OnColorChanged));
+
+    public static readonly DependencyProperty RedProperty = RegisterChannel(nameof(Red), 0);
+    public static readonly DependencyProperty GreenProperty = RegisterChannel(nameof(Green), 0);
+    public static readonly DependencyProperty BlueProperty = RegisterChannel(nameof(Blue), 0);
+    public static readonly DependencyProperty AlphaProperty = RegisterChannel(nameof(Alpha), byte.MaxValue);
+
     public static readonly DependencyProperty HexProperty = DependencyProperty.Register(
-        nameof(Hex), typeof(string), typeof(XColorPicker), new FrameworkPropertyMetadata("#00000000", FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, OnHexChanged));
+        nameof(Hex),
+        typeof(string),
+        typeof(XColorPicker),
+        new FrameworkPropertyMetadata(
+            "#FF000000",
+            FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+            OnHexChanged));
 
-    static XColorPicker() => DefaultStyleKeyProperty.OverrideMetadata(typeof(XColorPicker), new FrameworkPropertyMetadata(typeof(XColorPicker)));
+    static XColorPicker() => DefaultStyleKeyProperty.OverrideMetadata(
+        typeof(XColorPicker),
+        new FrameworkPropertyMetadata(typeof(XColorPicker)));
 
-    public Color SelectedColor { get => (Color)GetValue(SelectedColorProperty); set => SetValue(SelectedColorProperty, value); }
-    public byte Red { get => (byte)GetValue(RedProperty); set => SetValue(RedProperty, value); }
-    public byte Green { get => (byte)GetValue(GreenProperty); set => SetValue(GreenProperty, value); }
-    public byte Blue { get => (byte)GetValue(BlueProperty); set => SetValue(BlueProperty, value); }
-    public byte Alpha { get => (byte)GetValue(AlphaProperty); set => SetValue(AlphaProperty, value); }
-    public string Hex { get => (string)GetValue(HexProperty); set => SetValue(HexProperty, value); }
+    public Color SelectedColor
+    {
+        get => (Color)GetValue(SelectedColorProperty);
+        set => SetValue(SelectedColorProperty, value);
+    }
 
-    private static DependencyProperty RegisterChannel(string name) => DependencyProperty.Register(name, typeof(byte), typeof(XColorPicker), new FrameworkPropertyMetadata((byte)0, OnChannelChanged));
+    public byte Red
+    {
+        get => (byte)GetValue(RedProperty);
+        set => SetValue(RedProperty, value);
+    }
+
+    public byte Green
+    {
+        get => (byte)GetValue(GreenProperty);
+        set => SetValue(GreenProperty, value);
+    }
+
+    public byte Blue
+    {
+        get => (byte)GetValue(BlueProperty);
+        set => SetValue(BlueProperty, value);
+    }
+
+    public byte Alpha
+    {
+        get => (byte)GetValue(AlphaProperty);
+        set => SetValue(AlphaProperty, value);
+    }
+
+    public string Hex
+    {
+        get => (string)GetValue(HexProperty);
+        set => SetValue(HexProperty, value);
+    }
+
+    private static DependencyProperty RegisterChannel(string name, byte defaultValue) =>
+        DependencyProperty.Register(
+            name,
+            typeof(byte),
+            typeof(XColorPicker),
+            new FrameworkPropertyMetadata(defaultValue, OnChannelChanged));
 
     private static void OnColorChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs eventArgs)
     {
@@ -39,7 +89,9 @@ public class XColorPicker : Control
         XColorPicker picker = (XColorPicker)dependencyObject;
         if (!picker.synchronizing)
         {
-            picker.SelectedColor = Color.FromArgb(picker.Alpha, picker.Red, picker.Green, picker.Blue);
+            picker.SetCurrentValue(
+                SelectedColorProperty,
+                Color.FromArgb(picker.Alpha, picker.Red, picker.Green, picker.Blue));
         }
     }
 
@@ -48,24 +100,43 @@ public class XColorPicker : Control
         XColorPicker picker = (XColorPicker)dependencyObject;
         if (!picker.synchronizing && TryParse((string?)eventArgs.NewValue, out Color color))
         {
-            picker.SelectedColor = color;
+            picker.SetCurrentValue(SelectedColorProperty, color);
         }
     }
 
     private void SynchronizeFromColor(Color color)
     {
-        this.synchronizing = true;
-        SetCurrentValue(RedProperty, color.R);
-        SetCurrentValue(GreenProperty, color.G);
-        SetCurrentValue(BlueProperty, color.B);
-        SetCurrentValue(AlphaProperty, color.A);
-        SetCurrentValue(HexProperty, color.ToString());
-        this.synchronizing = false;
+        try
+        {
+            this.synchronizing = true;
+            this.SetCurrentValue(RedProperty, color.R);
+            this.SetCurrentValue(GreenProperty, color.G);
+            this.SetCurrentValue(BlueProperty, color.B);
+            this.SetCurrentValue(AlphaProperty, color.A);
+            this.SetCurrentValue(HexProperty, color.ToString());
+        }
+        finally
+        {
+            this.synchronizing = false;
+        }
     }
 
     private static bool TryParse(string? value, out Color color)
     {
-        try { color = (Color)ColorConverter.ConvertFromString(value ?? string.Empty); return true; }
-        catch (FormatException) { color = default; return false; }
+        try
+        {
+            color = (Color)ColorConverter.ConvertFromString(value ?? string.Empty);
+            return true;
+        }
+        catch (FormatException)
+        {
+            color = default;
+            return false;
+        }
+        catch (NotSupportedException)
+        {
+            color = default;
+            return false;
+        }
     }
 }
---
+++ VIA.WPF.Tests/Controls/Specialized/XColorPickerTests.cs
@@ -0,0 +1,161 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="XColorPickerTests.cs" company="VIA.WPF">
+//   Copyright (c) VIA.WPF. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Threading;
+using VIA.WPF.Controls;
+using VIA.WPF.Tests.Helpers;
+
+namespace VIA.WPF.Tests.Controls.Specialized;
+
+#region ### Class XColorPickerTests ###
+/// <summary>
+/// Provides tests for picker defaults, template inputs and binding preservation.
+/// </summary>
+public sealed class XColorPickerTests
+{
+    #region ### Public Methods ###
+    /// <summary>
+    /// Ensures that a new picker starts with an opaque black color.
+    /// </summary>
+    [Fact]
+    public void Defaults_ShouldBeOpaqueBlack()
+    {
+        WpfTestHelper.Run(
+            () =>
+            {
+                XColorPicker picker = new();
+
+                Assert.Equal(Colors.Black, picker.SelectedColor);
+                Assert.Equal((byte)0, picker.Red);
+                Assert.Equal((byte)0, picker.Green);
+                Assert.Equal((byte)0, picker.Blue);
+                Assert.Equal(byte.MaxValue, picker.Alpha);
+                Assert.Equal("#FF000000", picker.Hex);
+            });
+    }
+
+    /// <summary>
+    /// Ensures that the real template controls synchronize color channels and preserve the outer binding.
+    /// </summary>
+    [Fact]
+    public void TemplateInputs_ShouldSynchronizeColorAndPreserveBinding()
+    {
+        WpfTestHelper.Run(
+            () =>
+            {
+                ColorBindingSource source = new()
+                {
+                    Color = Colors.Black
+                };
+
+                XColorPicker picker = new();
+
+                BindingOperations.SetBinding(
+                    picker,
+                    XColorPicker.SelectedColorProperty,
+                    new Binding(nameof(ColorBindingSource.Color))
+                    {
+                        Source = source,
+                        Mode = BindingMode.TwoWay,
+                        UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
+                    });
+
+                Window host = new()
+                {
+                    Width = 560d,
+                    SizeToContent = SizeToContent.Height,
+                    Content = new Border
+                    {
+                        Padding = new Thickness(20d),
+                        Child = picker
+                    }
+                };
+
+                try
+                {
+                    host.Show();
+                    host.Dispatcher.Invoke(static () => { }, DispatcherPriority.Render);
+
+                    XSlider greenSlider = GetPart<XSlider>(picker, "PART_GreenSlider");
+                    XNumberBox redNumberBox = GetPart<XNumberBox>(picker, "PART_RedNumberBox");
+                    XNumberBox blueNumberBox = GetPart<XNumberBox>(picker, "PART_BlueNumberBox");
+                    XNumberBox alphaNumberBox = GetPart<XNumberBox>(picker, "PART_AlphaNumberBox");
+                    XTextBox hexTextBox = GetPart<XTextBox>(picker, "PART_HexTextBox");
+
+                    redNumberBox.ApplyTemplate();
+                    TextBox redEditor = Assert.IsType<TextBox>(
+                        redNumberBox.Template.FindName("PART_TextBox", redNumberBox));
+                    Button spinUpButton = Assert.IsType<Button>(
+                        redNumberBox.Template.FindName("PART_SpinUpButton", redNumberBox));
+
+                    Assert.Equal(TextAlignment.Right, redEditor.TextAlignment);
+
+                    redNumberBox.Text = "73";
+                    host.Dispatcher.Invoke(static () => { }, DispatcherPriority.DataBind);
+                    Assert.Equal(73d, redNumberBox.Value);
+                    Assert.Equal((byte)73, picker.Red);
+                    Assert.Equal((byte)73, source.Color.R);
+
+                    spinUpButton.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+                    host.Dispatcher.Invoke(static () => { }, DispatcherPriority.DataBind);
+                    Assert.Equal((byte)74, picker.Red);
+                    Assert.Equal((byte)74, source.Color.R);
+
+                    greenSlider.Value = 88d;
+                    host.Dispatcher.Invoke(static () => { }, DispatcherPriority.DataBind);
+                    Assert.Equal((byte)88, picker.Green);
+
+                    blueNumberBox.Text = "101";
+                    host.Dispatcher.Invoke(static () => { }, DispatcherPriority.DataBind);
+                    Assert.Equal((byte)101, picker.Blue);
+
+                    alphaNumberBox.Text = "128";
+                    host.Dispatcher.Invoke(static () => { }, DispatcherPriority.DataBind);
+                    Assert.Equal((byte)128, picker.Alpha);
+
+                    hexTextBox.Text = "#80445566";
+                    host.Dispatcher.Invoke(static () => { }, DispatcherPriority.DataBind);
+                    Assert.Equal(Color.FromArgb(128, 68, 85, 102), picker.SelectedColor);
+                    Assert.Equal(Color.FromArgb(128, 68, 85, 102), source.Color);
+                    Assert.NotNull(BindingOperations.GetBindingExpression(picker, XColorPicker.SelectedColorProperty));
+                }
+                finally
+                {
+                    host.Close();
+                }
+            });
+    }
+    #endregion
+
+    #region ### Private Methods ###
+    private static T GetPart<T>(Control control, string partName)
+        where T : DependencyObject
+    {
+        control.ApplyTemplate();
+        return Assert.IsType<T>(control.Template.FindName(partName, control));
+    }
+
+    private sealed class ColorBindingSource : DependencyObject
+    {
+        public static readonly DependencyProperty ColorProperty = DependencyProperty.Register(
+            nameof(Color),
+            typeof(Color),
+            typeof(ColorBindingSource),
+            new FrameworkPropertyMetadata(Colors.Black));
+
+        public Color Color
+        {
+            get => (Color)GetValue(ColorProperty);
+            set => SetValue(ColorProperty, value);
+        }
+    }
+    #endregion
+}
+#endregion
